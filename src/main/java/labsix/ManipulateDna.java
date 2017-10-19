d0ad862d3b838acf1c565e170ec0108b58f544e3
Jacob Walters <waltersj@aldenv183.allegheny.edu>
2017-10-19 14:24:52 -0400

    -f

--- src/main/java/labsix/ManipulateDna.java
+++ src/main/java/labsix/ManipulateDna.java
@@ -9,7 +9,7 @@ public class ManipulateDna {
 
   public static void main(String[] args) {
     // display the name of the programmer and the date
-    System.out.println("Gregory M. Kapfhammer " + new Date());
+    System.out.println("Jacob Walters " + new Date());
     // declare the starting file and scanner
     File dnaInputsFile = null;
     Scanner scanner = null;
@@ -21,17 +21,30 @@ public class ManipulateDna {
       System.out.println("Unable to locate file");
     }
     // Step One: Read in and display the chosen DNA string
-
+    String strand = scanner.nextLine();
+    System.out.println("Hello, your chosen DNA string is " + strand );
     // Step Two: Compute the complement of the DNA String
-
+    String mutatedStrand = strand.substring(0,4);
+    String upperCaseStrand = mutatedStrand.toUpperCase();
+    String complementStrand1 = upperCaseStrand.replace('A', 'T');
+    String complementStrand2 = complementStrand1.replace('T', 'A');
+    String complementStrand3 = complementStrand2.replace('C', 'G');
+    String complementStrand4 = complementStrand3.replace('G', 'C');
+    System.out.println("The complement of this string is " + complementStrand4 );
     // Step Three: Insert a randomly chosen DNA letter into the DNA String
-
+    Random insert = new Random();
+    char rdmLetter = chosenDNA.charAt(insert.nextInt(chosenDNA.legnth()));
+    System.out.println("Inserting " + rmdletter + " at position 0 produces" + rdmLetter + chosenDNA);
     // Step Four: Delete a DNA letter from a randomly chosen position in the DNA string
-
+    Random deletion = new Random();
+    num1 = deletion.nextInt(chosenDNA.legnth());
+    char delLetter = chosenDNA.charAt(num1);
+    mutation5 = chosenDNA.replace(delLetter, " ");
+    System.out.println("Deleting from position " + num1 + " gives" + mutation5);
     // Step Five: Change a random position in the DNA String to a randomly chosen DNA letter
-
+    String replace(rmdletter oldChar, rmdletter newChar);
     // Step Six: Display a final thankyou message
-
+    System.out.println("Thank you for using this program ");
   }
 
 }
